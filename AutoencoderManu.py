32d9f5ab4c00731c653cef9fef101144815b1292
EmanueleLedda97 <37103194+EmanueleLedda97@users.noreply.github.com>
2020-12-28 15:40:20 +0100

    Updated topologies

--- AutoencoderManu.py
+++ AutoencoderManu.py
@@ -3,63 +3,148 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Softmax, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Softmax, Dropout, Reshape
 from tensorflow.python.keras.layers import UpSampling2D
 from PIL import Image
 
 
 class AutoencoderManu:
 
-    def __init__(self, plot=True, loss='sparse_categorical_crossentropy', metrics=['accuracy']):
+    def __init__(self, folder='', topology=1, plot=True, loss='sparse_categorical_crossentropy'):
+
+        # This parameter is used to keep track of which autoencoder we are training
+        self.id_topology = topology
 
         # Setting dataset, checkpoint and other parameters
+        self.latent_dimension = 2048
         self.dataset = keras.datasets.cifar10
-        self.checkpoint_path = "cp_manu/cp.ckpt"
+        self.checkpoint_path = "cp_manu/" + folder + "/ae_" +\
+                               str(self.latent_dimension) +\
+                               "_test" +\
+                               str(self.id_topology) +\
+                               ".ckpt"
         self.opt = tf.keras.optimizers.Adam(lr=1e-4, decay=1e-5)
         self.plot = plot
+        self.metrics = ['accuracy']
 
+
+        # Declaring the models
+        self.encoder = Sequential()
+        self.decoder = Sequential()
+        self.autoencoder = Sequential()
+        self.classifier = Sequential()
         self.model = Sequential()
 
-        # Setting Encoder architecture
-        self.encoder = Sequential()
-        self.encoder.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))  # 3x32x32 -> 32x16x16
-        self.encoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-        self.encoder.add(MaxPool2D((2, 2), padding='same'))
+        if self.id_topology == 1:
+            # Setting Encoder architecture
+            self.encoder.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))  # 3x32x32 -> 32x16x16
+            self.encoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 32x16x16 -> 64x8x8
+            self.encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 128x4x4
+            self.encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+            self.encoder.add(Flatten())
+            self.encoder.add(Dense(self.latent_dimension, activation='relu'))
 
-        self.encoder.add(Dropout(0.2))
-        self.encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 32x16x16 -> 64x8x8
-        self.encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-        self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            # Setting Decoder architecture
+            self.decoder.add(Dense(8*8*128, activation='relu'))
+            self.decoder.add(Reshape((8, 8, 128)))
+            self.decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+            self.decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))  # 128x4x4 -> 64x8x8
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+            self.decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 32x16x16
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+            self.decoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+            self.decoder.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))  # 32x16x16 -> 3x32x32
 
-        self.encoder.add(Dropout(0.2))
-        self.encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 128x4x4
-        self.encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        if self.id_topology == 2:
+            # Setting Encoder architecture
+            self.encoder.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))  # 3x32x32 -> 32x16x16
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 32x16x16 -> 64x8x8
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 128x4x4
+            self.encoder.add(Flatten())
+            self.encoder.add(Dense(self.latent_dimension, activation='relu'))
 
+            # Setting Decoder architecture
+            self.decoder.add(Dense(8*8*128, activation='relu'))
+            self.decoder.add(Reshape((8, 8, 128)))
+            self.decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))  # 128x4x4 -> 64x8x8
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 32x16x16
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+            self.decoder.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))  # 32x16x16 -> 3x32x32
 
-        # Setting Decoder architecture
-        self.decoder = Sequential()
+        if self.id_topology == 3:
+            # Setting Encoder architecture
+            self.encoder.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))  # 3x32x32 -> 32x16x16
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 32x16x16 -> 64x8x8
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Flatten())
+            self.encoder.add(Dense(self.latent_dimension, activation='relu'))
 
-        self.decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-        self.decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))  # 128x4x4 -> 64x8x8
-        self.decoder.add(UpSampling2D((2, 2)))
+            # Setting Decoder architecture
+            self.decoder.add(Dense(8*8*64, activation='relu'))
+            self.decoder.add(Reshape((8, 8, 64)))
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 32x16x16
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+            self.decoder.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))  # 32x16x16 -> 3x32x32
 
-        self.decoder.add(Dropout(0.2))
-        self.decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-        self.decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 32x16x16
-        self.decoder.add(UpSampling2D((2, 2)))
+        if self.id_topology == 4:
+            # Setting Encoder architecture
+            self.encoder.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))  # 3x32x32 -> 32x16x16
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Flatten())
+            self.encoder.add(Dense(self.latent_dimension, activation='relu'))
 
-        self.decoder.add(Dropout(0.2))
-        self.decoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-        self.decoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-        self.decoder.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))  # 32x16x16 -> 3x32x32
+            # Setting Decoder architecture
+            self.decoder.add(Dense(16*16*32, activation='relu'))
+            self.decoder.add(Reshape((16, 16, 32)))
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+            self.decoder.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))  # 32x16x16 -> 3x32x32
+
+        if self.id_topology == 5:
+            # Setting Encoder architecture
+            self.encoder.add(Flatten())
+            self.encoder.add(Dense(self.latent_dimension, activation='relu'))
+
+            # Setting Decoder architecture
+            self.decoder.add(Dense(32 * 32 * 3, activation='relu'))
+            self.decoder.add(Reshape((32, 32, 3)))
+            self.decoder.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))  # 32x16x16 -> 3x32x32
+
+        # This one is a variant of the topology 4, which uses no dense layers
+        if self.id_topology == 6:
+            # Setting Encoder architecture
+            self.encoder.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))  # 3x32x32 -> 32x16x16
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 32x16x16 -> 64x8x8
+            self.encoder.add(MaxPool2D((2, 2), padding='same'))
+            self.encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 128x4x4
+
+            # Setting Decoder architecture
+            self.decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))  # 128x4x4 -> 64x8x8
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))  # 64x8x8 -> 32x16x16
+            self.decoder.add(UpSampling2D((2, 2)))
+            self.decoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+            self.decoder.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))  # 32x16x16 -> 3x32x32
 
         # Setting Autoencoder architecture
-        self.autoencoder = Sequential()
         self.autoencoder.add(self.encoder)
         self.autoencoder.add(self.decoder)
 
         # Setting Classifier architecture
-        self.classifier = Sequential()
         self.classifier.add(Flatten())
         self.classifier.add(Dense(64, activation='relu'))
         self.classifier.add(Dense(10, activation='softmax'))
@@ -69,8 +154,8 @@ class AutoencoderManu:
         self.model.add(self.classifier)
 
         # Compiling the model and the autoencoder
-        self.model.compile(optimizer=self.opt, loss=loss, metrics=metrics)
-        self.autoencoder.compile(optimizer=self.opt, loss=tf.keras.losses.MeanSquaredError(), metrics=metrics)
+        self.model.compile(optimizer=self.opt, loss=loss, metrics=self.metrics)
+        self.autoencoder.compile(optimizer=self.opt, loss=tf.keras.losses.MeanSquaredError(), metrics=self.metrics)
 
     def train(self, epochs=3):
 
@@ -93,6 +178,9 @@ class AutoencoderManu:
             plt.plot(history.history['val_accuracy'])
             plt.show()
 
+    def load_model(self):
+        self.autoencoder.load_weights(self.checkpoint_path)
+
     def train_autoencoder(self, epochs=3):
 
         # Setting up the checkpoint
@@ -111,32 +199,27 @@ class AutoencoderManu:
 
         # Plotting the results
         if self.plot:
-            plt.plot(history.history['accuracy'])
-            plt.plot(history.history['val_accuracy'])
+            plt.plot(history.history['loss'])
+            plt.plot(history.history['val_loss'])
+            plt.savefig('loss' + str(self.id_topology) + '.png')
             plt.show()
 
-    def encode(self, image):
+    def empiric_evaluation(self):
         (train_images, train_labels), (test_images, test_labels) = self.dataset.load_data()
-        print(train_images[0])
 
         train_images = train_images.astype('float64') / 255.
         test_images = test_images.astype('float64') / 255.
 
-        decoded_imgs = self.autoencoder.predict(train_images[:5])
+        encoded_imgs = self.encoder.predict(test_images)
+        decoded_imgs = self.decoder.predict(encoded_imgs)
 
-        # display reconstruction
-        plt.subplot(211)
-        plt.imshow(train_images[4].reshape(32, 32, 3))
-
-        plt.subplot(212)
-        plt.imshow(decoded_imgs[4].reshape(32, 32, 3))
-        plt.show()
-
-
-
-
-
-
+        for i in range(len(decoded_imgs)):
+            n_samples = 10
+            fig, axes = plt.subplots(2,n_samples)
+            for j in range(n_samples):
+                axes[0, j].imshow(test_images[i*n_samples+j].reshape(32, 32, 3))
+                axes[1, j].imshow(decoded_imgs[i*n_samples+j].reshape(32, 32, 3))
+            plt.show()
 
 
 
