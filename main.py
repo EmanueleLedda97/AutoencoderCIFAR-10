69d5d1202ca7ef065f986cdbec68fadbca828b4f
EmanueleLedda97 <37103194+EmanueleLedda97@users.noreply.github.com>
2020-12-28 15:41:57 +0100

    Updated main

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from AutoencoderManu import AutoencoderManu
-from AutoencoderGiaco import AutoencoderGiaco
 
-model_manu = AutoencoderManu(plot=True)
+# True if train mode is on
+train_mode = False
+fine_tuning = False
+topology = 5
 
-model_manu.train_autoencoder(20)
-model_manu.encode(2)
+# Creating the autoencoder
+mod = AutoencoderManu(folder='conv_variant', topology=6, plot=True)
+
+# Training/Evaluating the model
+if train_mode:
+    if fine_tuning:
+        mod.load_model()
+    mod.train_autoencoder(epochs=250)
+else:
+    mod.load_model()
+    mod.empiric_evaluation()
